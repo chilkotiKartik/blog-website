bd54cb894a0ae451c6054aa63248e8c1d695749c
chilkotikartik <chilkotikartik@gmail.com>
2026-09-16 20:23:23 +0530

    refactor(alert): add AlertProps interface and custom message support
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,40 +1,33 @@
 import Container from './container'
-import cn from 'classnames'
 import { EXAMPLE_PATH } from '../lib/constants'
-import Link from "next/link";
 
-export default function Alert({ preview }) {
+export interface AlertProps {
+  preview?: boolean;
+  message?: string;
+  className?: string;
+}
+
+export default function Alert({ preview, message, className = '' }: AlertProps) {
+  if (!preview && !message) return null;
   return (
     <div
-      className={cn('border-b', {
-        'bg-accent-7 border-accent-7 text-white': preview,
-        'bg-accent-1 border-accent-2': !preview,
-      })}
+      className={order-b  }
     >
       <Container>
         <div className="py-2 text-center text-sm">
           {preview ? (
             <>
               This is a page preview.{' '}
-              <Link
+              <a
                 href="/api/exit-preview"
                 className="underline hover:text-cyan duration-200 transition-colors"
               >
                 Click here
-              </Link>{' '}
+              </a>{' '}
               to exit preview mode.
             </>
           ) : (
-            <>
-              The source code for this blog is{' '}
-              <Link
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-success duration-200 transition-colors"
-              >
-                available on GitHub
-              </Link>
-              .
-            </>
+            <span>{message}</span>
           )}
         </div>
       </Container>
