2a1593b05235739bf9c2ea5387c18cbc81f02db6
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 22:01:07 +0530

    refactor(components): add ContainerProps interface with className support and children typing
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/container.tsx
+++ components/container.tsx
@@ -1,3 +1,10 @@
-export default function Container({ children }) {
-  return <div className="container mx-auto px-5">{children}</div>
+import { ReactNode } from 'react'
+
+export interface ContainerProps {
+  children: ReactNode
+  className?: string
+}
+
+export default function Container({ children, className = '' }: ContainerProps) {
+  return <div className={`container mx-auto px-5 ${className}`.trim()}>{children}</div>
 }
