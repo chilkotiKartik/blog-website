9606dc83ebf53b6a44a73057edc9207792331ca2
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 16:17:31 +0530

    refactor(components): add LayoutProps TypeScript interface with meta forwarding
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,12 +1,20 @@
+import { ReactNode } from 'react'
 import Alert from './alert'
 import Footer from './footer'
-import Meta from './meta'
+import Meta, { MetaProps } from './meta'
 
-export default function Layout({ preview, children }) {
+export interface LayoutProps {
+  preview?: boolean;
+  children: ReactNode;
+  metaProps?: MetaProps;
+  className?: string;
+}
+
+export default function Layout({ preview, children, metaProps, className }: LayoutProps) {
   return (
     <>
-      <Meta />
-      <div className="min-h-screen">
+      <Meta {...metaProps} />
+      <div className={`min-h-screen ${className || ''}`}>
         {/* <Alert preview={preview} /> */}
         <main>{children}</main>
       </div>
