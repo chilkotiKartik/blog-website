9ffe3adbafc9ed2ccb8d27377d6f0dff959064e7
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 20:22:20 +0530

    refactor(components): add MetaProps interface, theme color, and OpenGraph fallbacks to Meta component
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,42 +1,51 @@
 import Head from 'next/head'
 import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 
-export default function Meta() {
+export interface MetaProps {
+  title?: string
+  description?: string
+  ogImage?: string
+}
+
+export default function Meta({
+  title,
+  description = `A statically generated blog example using Next.js and ${CMS_NAME}.`,
+  ogImage = HOME_OG_IMAGE_URL,
+}: MetaProps) {
   return (
     <Head>
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/favicon/Group.png"
+        href="/favicon/apple-touch-icon.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/favicon/Group.png"
+        href="/favicon/favicon-32x32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/favicon/Group.png"
+        href="/favicon/favicon-16x16.png"
       />
       <link rel="manifest" href="/favicon/site.webmanifest" />
       <link
         rel="mask-icon"
-        href="/favicon/Group.svg"
+        href="/favicon/safari-pinned-tab.svg"
         color="#000000"
       />
-      <link rel="shortcut icon" href="/favicon/Group" />
+      <link rel="shortcut icon" href="/favicon/favicon.ico" />
       <meta name="msapplication-TileColor" content="#000000" />
       <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
       <meta name="theme-color" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
-      <meta
-        name="description"
-        content={`Kepoy Blog`}
-      />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta name="description" content={description} />
+      <meta property="og:image" content={ogImage} />
+      {title && <meta property="og:title" content={title} />}
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   )
 }
