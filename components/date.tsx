efb45b11f401f270f9f019042a8354b21238359a
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 22:20:37 +0530

    refactor(components): add DateProps interface and defensive date parsing in Date
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/date.tsx
+++ components/date.tsx
@@ -1,6 +1,22 @@
-import { parseISO, format } from 'date-fns'
+import { parseISO, format, isValid } from 'date-fns'
 
-export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL	d, yyyy')}</time>
+export interface DateProps {
+  dateString: string
+  formatPattern?: string
+}
+
+export default function Date({ dateString, formatPattern = 'LLLL d, yyyy' }: DateProps) {
+  if (!dateString) {
+    return null
+  }
+
+  try {
+    const date = parseISO(dateString)
+    if (!isValid(date)) {
+      return <time dateTime={dateString}>{dateString}</time>
+    }
+    return <time dateTime={dateString}>{format(date, formatPattern)}</time>
+  } catch {
+    return <time dateTime={dateString}>{dateString}</time>
+  }
 }
