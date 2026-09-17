60787feb0f4a8ecd0d09b14b66a6c82f24cff678
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 23:00:42 +0530

    refactor(pages): add IndexProps interface and safe excerpt helper in Home page
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,37 +3,63 @@ import { GetStaticProps } from 'next'
 import Container from '../components/container'
 import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
 import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
 import Header from '../components/header'
 
-export default function Index({ allPosts: { edges }, preview }) {
-  const heroPost = edges[0]?.node
-  const excerpt = getExcerpt(edges[0]?.node.excerpt); 
-  const morePosts = edges.slice(1) 
-
-  function getExcerpt(content) {
-    const maxWords = 50;
-    // Split the content into an array of words
-    const words = content.split(' ');
-  
-    // Ensure the excerpt does not exceed the maximum number of words
-    if (words.length > maxWords) {
-      return words.slice(0, maxWords).join(' ') + '...';
+export interface PostNode {
+  title: string
+  featuredImage?: {
+    node?: {
+      sourceUrl?: string
     }
-  
-    return content;
+  }
+  date: string
+  author?: {
+    node?: {
+      name: string
+      picture?: {
+        url?: string
+      }
+    }
+  }
+  slug: string
+  excerpt?: string
+}
+
+export interface PostEdge {
+  node: PostNode
+}
+
+export interface IndexProps {
+  allPosts?: {
+    edges?: PostEdge[]
+  }
+  preview?: boolean
+}
+
+export default function Index({ allPosts, preview = false }: IndexProps) {
+  const edges = allPosts?.edges ?? []
+  const heroPost = edges[0]?.node
+  const excerpt = getExcerpt(heroPost?.excerpt)
+  const morePosts = edges.slice(1)
+
+  function getExcerpt(content?: string) {
+    if (!content) return ''
+    const maxWords = 50
+    const words = content.split(' ')
+    if (words.length > maxWords) {
+      return words.slice(0, maxWords).join(' ') + '...'
+    }
+    return content
   }
 
   return (
     <Layout preview={preview}>
       <Head>
-        <title>{`Keploy`}</title>
+        <title>Keploy Blog</title>
       </Head>
       <Container>
-        {/* <Intro /> */}
         <Header />
         {heroPost && (
           <HeroPost
@@ -55,7 +81,7 @@ export const getStaticProps: GetStaticProps = async ({ preview = false }) => {
   const allPosts = await getAllPostsForHome(preview)
 
   return {
-    props: { allPosts, preview },
+    props: { allPosts: allPosts ?? { edges: [] }, preview },
     revalidate: 10,
   }
 }
