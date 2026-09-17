bad61964695ff7ee4b05d75e0738c00228e25226
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 21:40:46 +0530

    refactor(components): add LayoutProps interface, children typing, and optional meta forwarding
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,13 +1,20 @@
+import { ReactNode } from 'react'
 import Alert from './alert'
 import Footer from './footer'
-import Meta from './meta'
+import Meta, { MetaProps } from './meta'
 
-export default function Layout({ preview, children }) {
+export interface LayoutProps {
+  preview?: boolean
+  children: ReactNode
+  meta?: MetaProps
+}
+
+export default function Layout({ preview = false, children, meta }: LayoutProps) {
   return (
     <>
-      <Meta />
+      <Meta {...meta} />
       <div className="min-h-screen">
-        {/* <Alert preview={preview} /> */}
+        {preview && <Alert preview={preview} />}
         <main>{children}</main>
       </div>
       <Footer />
