e7c3272aec10b073bef6d50c55bb3f504155c4c7
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 09:55:39 +0530

    refactor(meta): add MetaProps interface with customizable title, description, and ogImage
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/meta.tsx
+++ components/meta.tsx
@@ -1,7 +1,17 @@
 import Head from 'next/head'
 import { CMS_NAME, HOME_OG_IMAGE_URL } from '../lib/constants'
 
-export default function Meta() {
+export interface MetaProps {
+  title?: string;
+  description?: string;
+  ogImage?: string;
+}
+
+export default function Meta({
+  title = 'Keploy Blog',
+  description = 'Keploy Blog',
+  ogImage = HOME_OG_IMAGE_URL,
+}: MetaProps = {}) {
   return (
     <Head>
       <link
@@ -34,9 +44,11 @@ export default function Meta() {
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
       <meta
         name="description"
-        content={`Kepoy Blog`}
+        content={description}
       />
-      <meta property="og:image" content={HOME_OG_IMAGE_URL} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={ogImage} />
     </Head>
   )
 }
