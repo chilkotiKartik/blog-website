32eaef739684ec6b42094cc072f8800c27b8eb49
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 22:01:14 +0530

    refactor(components): add PostHeaderProps TypeScript interface and responsive typography
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/post-header.tsx
+++ components/post-header.tsx
@@ -1,8 +1,20 @@
-import Avatar from './avatar'
-import Date from './date'
-import CoverImage from './cover-image'
+import Avatar, { AvatarAuthor } from './avatar'
+import DateComponent from './date'
+import CoverImage, { CoverImageNode } from './cover-image'
 import PostTitle from './post-title'
-import Categories from './categories'
+import Categories, { CategoriesProps } from './categories'
+
+export interface PostHeaderProps {
+  title: string
+  coverImage?: {
+    node?: CoverImageNode
+  }
+  date: string
+  author?: {
+    node?: AvatarAuthor
+  } | AvatarAuthor
+  categories?: CategoriesProps['categories']
+}
 
 export default function PostHeader({
   title,
@@ -10,23 +22,29 @@ export default function PostHeader({
   date,
   author,
   categories,
-}) {
+}: PostHeaderProps) {
+  const resolvedAuthor: AvatarAuthor | undefined = author
+    ? 'node' in author && author.node
+      ? author.node
+      : (author as AvatarAuthor)
+    : undefined
+
   return (
     <>
       <PostTitle>{title}</PostTitle>
       <div className="hidden md:flex flex-col items-center justify-center md:mb-12">
-        <Avatar author={author} />
-        <div className="mb-6 text-lg">
-          <Date dateString={date} />
-          <Categories categories={categories} />
+        {resolvedAuthor && <Avatar author={resolvedAuthor} />}
+        <div className="mb-6 text-lg flex items-center gap-2 mt-3">
+          <DateComponent dateString={date} />
+          {categories && <Categories categories={categories} />}
         </div>
       </div>
       <div className="mb-8 md:mb-16 sm:mx-0 xl:w-2/3 md:w-4/5 w-full md:-translate-x-1/2 md:left-1/2 relative">
-        <CoverImage title={title} coverImage={coverImage} />
+        <CoverImage title={title} coverImage={coverImage} priority={true} />
       </div>
       <div className="max-w-2xl mx-auto">
         <div className="block md:hidden mb-6">
-          <Avatar author={author} />
+          {resolvedAuthor && <Avatar author={resolvedAuthor} />}
         </div>
       </div>
     </>
