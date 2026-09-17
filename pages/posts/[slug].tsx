30825a34b05df72f95e0f130b5abfca5f2cdd624
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 16:18:36 +0530

    refactor(pages): add PostProps interface and remove redundant console.log
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -14,10 +14,15 @@ import Tags from '../../components/tags'
 import { getAllPostsWithSlug, getPostAndMorePosts } from '../../lib/api'
 import { CMS_NAME } from '../../lib/constants'
 
-export default function Post({ post, posts, preview }) {
+export interface PostProps {
+  post: any;
+  posts: any;
+  preview?: boolean;
+}
+
+export default function Post({ post, posts, preview }: PostProps) {
   const router = useRouter()
   const morePosts = posts?.edges
-  console.log();
 
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />
