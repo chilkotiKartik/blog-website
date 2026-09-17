600a342fed10df1102037574352a939daa6324ae
chilkotikartik <chilkotikartik@gmail.com>
2026-09-17 16:11:49 +0530

    refactor(components): add CoverImageProps and PostTitleProps interfaces with styling support
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- components/cover-image.tsx
+++ components/cover-image.tsx
@@ -2,17 +2,18 @@ import cn from 'classnames'
 import Image from 'next/image'
 import Link from 'next/link'
 
-interface Props {
-  title: string
+export interface CoverImageProps {
+  title: string;
   coverImage: {
     node: {
-      sourceUrl: string
-    }
-  }
-  slug?: string
+      sourceUrl: string;
+    };
+  };
+  slug?: string;
+  className?: string;
 }
 
-export default function CoverImage({ title, coverImage, slug }: Props) {
+export default function CoverImage({ title, coverImage, slug, className }: CoverImageProps) {
   const image = (
     <Image
       width={2000}
