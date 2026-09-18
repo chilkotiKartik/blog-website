4077036aed4be76a74b6caa9f9a2421c1915171a
chilkotikartik <chilkotikartik@gmail.com>
2026-09-18 06:18:04 +0530

    refactor(document): add charset meta and preconnect resource hints in Document component
    Signed-off-by: chilkotikartik <chilkotikartik@gmail.com>

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <meta charSet="utf-8" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      </Head>
+      <body className="antialiased bg-white text-accent-8 selection:bg-orange-500 selection:text-white">
         <Main />
         <NextScript />
       </body>
